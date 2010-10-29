dcaff6ad29a2f1715bd236da4279dccc9b932e34
Simo Niemelä <simo.niemela@gmail.com>
2010-10-30 00:51:25 +0300

    First commit

---
+++ swedish_language_pack_extension.rb
@@ -0,0 +1,8 @@
+class SwedishLanguagePackExtension < Radiant::Extension
+  version "1.0"
+  description "Swedish language pack for Radiant CMS"
+  url "http://yourwebsite.com/se"
+    
+  def activate
+  end
+end
